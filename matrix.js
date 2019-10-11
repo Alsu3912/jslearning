c812de429d8c3ba6a9ed7f92536e34f06aa5d62c
Alsu3912 <alsu3912@gmail.com>
2019-10-11 13:46:42 +0200

    The text of exception is changed, some variables is renamed

--- matrix.js
+++ matrix.js
@@ -1,17 +1,17 @@
 var matrix = function (matrix1, matrix2) {
-    var iLength = matrix1.length - 1;
-    var jLength = matrix1[0].length - 1;
-    var kLength = matrix2.length - 1;
-    var lLength = matrix2[0].length - 1;
+    var matrix1row = matrix1.length - 1;
+    var matrix1column = matrix1[0].length - 1;
+    var matrix2row = matrix2.length - 1;
+    var matrix2column = matrix2[0].length - 1;
     var multResult = [];
-    if (jLength != kLength) {
-        throw "Exception";
+    if (matrix1column != matrix2row) {
+        throw "Multiplication of these matrices is impossible. The number of columns of the first matrix should be equal to the number of second matrix";
     }
-    for (var i = 0; i <= iLength; i++) {
+    for (var i = 0; i <= matrix1row; i++) {
         multResult.push([]);
-        for (var l = 0; l <= lLength; l++) {
+        for (var l = 0; l <= matrix2column; l++) {
             multResult[i][l] = 0;
-            for (var j = 0; j <= jLength; j++) {
+            for (var j = 0; j <= matrix1column; j++) {
                 multResult[i][l] = multResult[i][l] + matrix1[i][j] * matrix2[j][l];
             }
         }
