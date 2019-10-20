53ad661b90e9dae467107b6a7ccee97272d16df5
Alsu3912 <alsu3912@gmail.com>
2019-10-20 14:13:11 +0200

    Rid of a code duplication

--- zipArr.js
+++ zipArr.js
@@ -1,19 +1,19 @@
 var zipArrays = function (arr1, arr2, transform) {
     var resultArr = [];
+    var largerArr = [], 
+        smallerArr = [];
     if (arr1.length >= arr2.length) {
-        for (let x = 0; x <= arr2.length - 1; x++) {
-            resultArr.push(transform(arr1[x], arr2[x]));
-        }
-        for (let i = arr2.length; i <= arr1.length - 1; i++) {
-            resultArr.push(arr1[i]);
-        }
-    } else if (arr1.length < arr2.length) {
-        for (let x = 0; x <= arr1.length - 1; x++) {
-            resultArr.push(transform(arr1[x], arr2[x]));
-        }
-        for (let j = arr1.length; j <= arr2.length - 1; j++) {
-            resultArr.push(arr2[j]);
-        }
+        largerArr = arr1;
+        smallerArr = arr2;
+    } else {
+        largerArr = arr2;
+        smallerArr = arr1;
+    }
+    for (let x = 0; x <= smallerArr.length - 1; x++) {
+        resultArr.push(transform(largerArr[x], smallerArr[x]));
+    }
+    for (let i = smallerArr.length; i <= largerArr.length - 1; i++) {
+        resultArr.push(largerArr[i]);
     }
     return resultArr;
 }
